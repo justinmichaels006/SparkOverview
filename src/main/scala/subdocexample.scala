b0f957cf35d3db3bb14de36a7c16e3b66af632a0
justinmichaels006 <justin@couchbase.com>
2017-09-07 13:51:53 -0700

    add subdoc

---
+++ src/main/scala/subdocexample.scala
@@ -0,0 +1,41 @@
+import com.couchbase.client.java.document.JsonDocument
+import org.apache.spark.sql.SparkSession
+import com.couchbase.spark._
+
+/**
+  * Created by justin on 9/7/17.
+  */
+object quickstart {
+  def main(sSess:SparkSession) = {
+
+    val sc = sSess.sparkContext
+    val result = sc.parallelize(Seq("airline_10"))
+      .couchbaseSubdocLookup(Seq("name", "iata"), exists = Seq("type"))
+      .collect()
+
+    result.foreach(println)
+    // Prints
+    // SubdocLookupResult(
+    //    airline_10,0,Map(name -> 40-Mile Air, iata -> Q5)
+    // )
+
+    val r3 = sc
+      .parallelize(Seq("airline_10")) // Define Document IDs
+      .couchbaseGet[JsonDocument]() // Load document from Couchbase
+      .map(_.content()) // extract the content
+      .collect() // collect all data
+      .foreach(println) // print it out
+    // Prints
+    // airline_10 document
+    //{
+    //"id": 10,
+    //"type": "airline",
+    //"name": "40-Mile Air",
+    //"iata": "Q5",
+    //"icao": "MLA",
+    //"callsign": "MILE-AIR",
+    //"country": "United States"
+    //}
+
+  }
+}
